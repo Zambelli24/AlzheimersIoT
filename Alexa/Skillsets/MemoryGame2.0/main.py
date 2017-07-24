287d36251d9633dbc5d0f4506e35354e5848a03e
Adam Howard <stalh05@moravian.edu>
2017-07-24 09:17:41 -0400

    Memory Game 2.0 commit

---
+++ Alexa/Skillsets/MemoryGame2.0/main.py
@@ -0,0 +1,49 @@
+import logging
+from game_process_logic import *
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+def process_state(user_answer):
+    round = counter_dict.get("rounds")
+    if round >= 5:
+        message = quit_game()
+        return message, 1
+    if user_answer == 'yes':
+        print(user_answer)
+        message = yes_response()
+        return message, 0
+    if user_answer == 'no':
+        message = quit_game()
+        return message, 1
+    if user_answer == 'pass':
+        message = pass_response()
+        return message, 0
+    else:
+        message = answer_response(user_answer)
+        return message, 0
+
+
+@ask.launch
+def new_game():
+    counter_dict['wins'] = 0
+    counter_dict['rounds'] = 0
+    welcome_msg = render_template('welcome')
+    return question(welcome_msg)
+
+
+@ask.intent("AnswerIntent", convert={'Answer': str})
+def main_intent(answer):
+    user_answer = answer
+    new_msg, end = process_state(user_answer)
+    if end == 0:
+        return question(new_msg)
+    else:
+        return statement(new_msg)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
